8cac0ffa6a0973da57ca609f211ea7f3b8b34509
rx <1003142361@qq.com>
2018-11-19 21:05:26 +0800

    完成home页录播图数据请求及渲染,完成分类列表

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//引入vue-resource
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
 
 Vue.config.productionTip = false
 
 //按需引入mint-ui
-import { Header } from 'mint-ui'
+import { Header , Swipe, SwipeItem} from 'mint-ui'
 Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
 //引入mui
 import './lib/mui/css/mui.css'
 import './lib/mui/css/icons-extra.css'
